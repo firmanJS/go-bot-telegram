8398fb1cf5cdaffc22abdda236a8078612ee38a5
firmanjs <fimzhakim@gmail.com>
2020-10-05 15:42:22 +0700

    refactoring code

---
+++ utils/webhook.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/firmanJS/go-bot-telegram/models"
+)
+
+// This function is called whenever an update is recieved
+func WebHookHandler(rw http.ResponseWriter, req *http.Request) {
+	// Create our web hook request body type instance
+	body := &models.WebHookReqBody{}
+
+	// Decodes the incoming request into our cutom webhookreqbody type
+	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
+		log.Printf("An error occured (webHookHandler)")
+		log.Panic(err)
+		return
+	}
+
+	fmt.Println("sending request")
+
+	action := strings.ToLower(body.Message.Text)
+	chatId := body.Message.Chat.ID
+
+	SendingReply(chatId, action)
+}
+
+func SendingReply(chatID int64, command string) error {
+	//get message in request command
+	msg := RequestCommand(command)
+
+	reqBody := &models.SendMessageReqBody{
+		ChatID: chatID,
+		Text:   msg,
+	}
+
+	// Convert our custom type into json format
+	reqBytes, err := json.Marshal(reqBody)
+
+	if err != nil {
+		return err
+	}
+
+	RequestApiTelegram(reqBytes)
+
+	return err
+
+}
